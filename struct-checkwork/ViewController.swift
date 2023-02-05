684c19b18eb449ea9a7ccc4437a40904b1ac1fcc
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-02-05 10:14:41 +0900

    Initial Commit

---
+++ struct-checkwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  struct-checkwork
+//
+//  Created by JotaroSugiyama on 2023/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
